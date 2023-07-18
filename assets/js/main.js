9634ca3cef0b766409cc932c65527220798d294b
silverskg <chokichoki_gm@i.softbank.jp>
2023-07-18 21:38:38 +0900

    main-content/add

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,4 +24,39 @@ $(document).ready(function () {
     pauseOnFocus: false,
     pauseOnHover: false,
   });
+
+  var swiper = new Swiper(".swiper-container", {
+    slidesPerView: "auto",
+    loop: true,
+    centeredSlides: true,
+    spaceBetween: 16,
+    autoplay:{
+      delay: 4000,
+    },
+
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+  var swiper = new Swiper(".event-slide", {
+    slidesPerView: "auto",
+    loop: true,
+    centeredSlides: true,
+    // spaceBetween: 16,
+    autoplay:{
+      delay: 4000,
+    },
+    slidesPerView: 2,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+
 });
