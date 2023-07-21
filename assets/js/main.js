80863d9cce9ed26de5c391a83f478011bff22e53
silverskg <chokichoki_gm@i.softbank.jp>
2023-07-21 12:36:40 +0900

    レスポンシブ対応

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,35 +1,11 @@
 $(document).ready(function () {
-  // スライダーの設定
-  var slider = $(".slick-slider");
-  slider.slick({
-    dots: true,
-    infinite: true,
-    speed: 300,
-    slidesToShow: 1,
-    adaptiveHeight: true,
-    autoplay: true,
-    autoplaySpeed: 3000,
-    fade: true,
-    cssEase: "linear",
-  });
 
-  // full-screen スライダーの設定
-  $(".full-screen").slick({
-    centerMode: true,
-    centerPadding: "5%",
-    dots: true,
-    autoplay: true,
-    autoplaySpeed: 2800,
-    infinite: true,
-    pauseOnFocus: false,
-    pauseOnHover: false,
-  });
-
-  var swiper = new Swiper(".swiper-container", {
+  var swiper1 = new Swiper(".swiper-container", {
     slidesPerView: "auto",
     loop: true,
+    spaceBetween: 60,
+    loopAdditionalSlides: 1,
     centeredSlides: true,
-    spaceBetween: 16,
     autoplay: {
       delay: 4000,
     },
@@ -40,13 +16,16 @@ $(document).ready(function () {
     },
   });
 
-  var swiper = new Swiper(".event-slide", {
-    slidesPerView: 2,
-    // loop: true,
+  var swiper2 = new Swiper(".event-slide", {
+    slidesPerView: "auto",
+    loop: true,
+    spaceBetween: 30,
+    loopAdditionalSlides: 1,
     centeredSlides: true,
     autoplay: {
-      delay: 14000,
+      delay: 4000,
     },
+
     navigation: {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
@@ -56,24 +35,8 @@ $(document).ready(function () {
       type: "bullets",
       clickable: true,
       dynamicBullets: true,
-      dynamicMainBullets: 1,
-    },
-    breakpoints: {
-      960: {
-        slidesPerView: 1,
-      },
-    },
+      dynamicMainBullets: 2,
 
-    on: {
-      slideChangeTransitionEnd: function () {
-        var slides = document.querySelectorAll(".swiper-slide");
-        slides.forEach((slide) => {
-          slide.classList.remove("active-slide");
-        });
-
-        var activeSlide = slides[this.realIndex + 1]; // +1 to account for cloned slide in loop mode
-        activeSlide.classList.add("active-slide");
-      },
     },
   });
 });
